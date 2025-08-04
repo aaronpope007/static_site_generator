93884653a31e661f2275ba141451ef2117303ee3
Aaron Pope <aaronpope007@gmail.com>
2025-08-04 07:24:07 -0500

    added node/tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,17 +1,16 @@
-
-
 class HTMLNode:
-    def __init__(self, text, text_type, url=None):
-        self.text = text
-        self.text_type = text_type
-        self.url = url
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
 
-    def __eq__(self, other):
-        return (
-            self.text_type == other.text_type
-            and self.text == other.text
-            and self.url == other.url
-        )
+    def to_html(self):
+        raise NotImplementedError
 
-    def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+
+        props_list = [f'{key}="{value}"' for key, value in self.props.items()]
+        return " ".join(props_list)
