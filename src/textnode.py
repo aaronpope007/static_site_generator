e0cfebbf8d273b8d12d6882e2d3aca6465fe4b11
Aaron Pope <aaronpope007@gmail.com>
2025-08-04 06:53:03 -0500

    updated for unit testing, clean up

--- src/textnode.py
+++ src/textnode.py
@@ -1,36 +1,27 @@
 from enum import Enum
 
+
 class TextType(Enum):
-    PLAIN_TEXT = "plain_text"
-    BOLD_TEXT = "bold_text"
-    ITALIC_TEXT = "italic_text"
-    CODE_TEXT = "code_text"
-    LINK_TEXT = "link_text"
-    IMAGE_TEXT = "image_text"
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
-        if self.text_type in (TextType.LINK_TEXT, TextType.IMAGE_TEXT):
-            if not self.url or not self.url.startswith("http"):
-                raise ValueError("A valid URL must be provided for links and images.")
 
     def __eq__(self, other):
-        if isinstance(other, TextNode):
-            return self.text == other.text and self.text_type == other.text_type and self.url == other.url
-            
-        else:
-            return False
-            
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
 
-        #Create an __eq__ method that returns True if all of the properties of 
-        #two TextNode objects are equal. 
-        #Our future unit tests will rely on this method to compare objects.
-        
-    def __repr__():
-        #method returns a string representation of the TextNode object
-        #should look like this: TextNode(TEXT, TEXT_TYPE, URL)
-
-
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
