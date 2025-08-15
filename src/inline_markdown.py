7016ebf36ef016dc4c03b14222cd5e56e7960d8d
Aaron Pope <aaronpope007@gmail.com>
2025-08-15 09:40:53 -0500

    added delimiter info to error message

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -12,7 +12,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         split_nodes = []
         sections = old_node.text.split(delimiter)
         if len(sections) % 2 == 0:
-            raise ValueError("invalid markdown, formatted section not closed")
+            raise ValueError(f"invalid markdown, formatted {delimiter} section not closed")
         for i in range(len(sections)):
             if sections[i] == "":
                 continue
