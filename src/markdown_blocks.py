b3942584a6c6629e5c60a2fb7ecd817ebd391f23
Aaron Pope <aaronpope007@gmail.com>
2025-08-16 11:22:03 -0500

    handling block types

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -44,17 +44,30 @@ def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
     block_nodes = []
 
+
     for block in blocks:
         block_type = block_to_block_type(block)
 
         if block_type == BlockType.PARAGRAPH:
             block = text_to_textnodes(block)
+            child_nodes = []
             for text_node in block:
-                text_node = text_node_to_html_node(text_node)
+                child_nodes.append(text_node_to_html_node(text_node))
+                #creates and appends paragraph node
+            block_nodes.append(HTMLNode("p", None, child_nodes, None))
+
+        elif block_type == BlockType.HEADING:
+            heading_num = re.match(r'#*', block)
+            cleaned_block = block.lstrip('#')
+            cleaned_block = text_to_textnodes(cleaned_block)
+            child_nodes = []
+            for text_node in block:
+                child_nodes.append(text_node_to_html_node(text_node))
+                block_nodes.append(HTMLNode(f'h{heading_num}', None, child_nodes, None))
 
 
-
-
+    return HTMLNode("div", None, block_nodes, None)
+    #need: heading, code, quote, etc
     # 1. split markdown into blocks
     # 2. based on type of block, create a new HTMLNode w proper data
     # 3. assign proper child HTMLNode objects to the block node
