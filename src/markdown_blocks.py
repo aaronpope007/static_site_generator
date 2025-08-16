993930920cc4ad8eeb5d79e6058e7a0ef9bd2b16
Aaron Pope <aaronpope007@gmail.com>
2025-08-16 10:36:18 -0500

    adding markdown to html logic

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,6 +1,8 @@
 import re
 from enum import Enum
-
+from htmlnode import HTMLNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -28,8 +30,6 @@ def block_to_block_type(block):
 
     return BlockType.PARAGRAPH
 
-
-
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -39,3 +39,29 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    block_nodes = []
+
+    for block in blocks:
+        block_type = block_to_block_type(block)
+
+        if block_type == BlockType.PARAGRAPH:
+            block = text_to_textnodes(block)
+            for text_node in block:
+                text_node = text_node_to_html_node(text_node)
+
+
+
+
+    # 1. split markdown into blocks
+    # 2. based on type of block, create a new HTMLNode w proper data
+    # 3. assign proper child HTMLNode objects to the block node
+    #   use text to children(text) function
+    #   textnode ->htmlnode
+    # 4. code block is special case - dont use text to children
+    #
+    # 5. make all block nodes children under a single
+    # parent HTML node (which should just be a div) and return it
+    # 6. make unit tests
