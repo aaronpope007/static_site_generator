09466926d8c676c29b362188c16971ba2d86d6a4
Aaron Pope <aaronpope007@gmail.com>
2025-08-20 07:59:06 -0500

    add fx to main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from textnode import TextNode, TextType
+from copystatic import copy_static
 
 
 def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
 
+    copy_static('static', 'public')
 
 main()
