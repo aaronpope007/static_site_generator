0878f27af8c839f67125d5172df4eb3110dc6c09
Aaron Pope <aaronpope007@gmail.com>
2025-08-21 08:55:08 -0500

    extract title function

--- src/extract_title.py
+++ src/extract_title.py
@@ -1,1 +1,11 @@
+
 def extract_title(markdown):
+    markdown_array = markdown.split('\n')
+    header = ''
+
+    for line in markdown_array:
+        if line.startswith('# '):
+            return line.lstrip('# ').rstrip()
+
+    if header == '':
+        raise Exception('This document needs a header: #')
